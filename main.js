943cdf95079f52ff572f64be0a6fa170052afb28
LateAlways <90992352+Sky-FoxGAMING@users.noreply.github.com>
2022-08-04 15:29:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 setTimeout(() => {
                     wait = animate("Hello " + data + "! ")
                     setTimeout(() => {
-                        fetch("https://https://ipapi.co/" + ip + "/json/")
+                        fetch("https://ipapi.co/" + ip + "/json/")
                             .then((r) => r.json())
                             .then(data => {
                                 instAnim("<br><span style=\"color: rgba(50,255,50,255);\">" + ip + "@me.fuchsiax.dev ~ $</span> ", false)
