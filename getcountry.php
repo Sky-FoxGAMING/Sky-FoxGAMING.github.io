6dbe20f0f88bdd6d4af48a84ebb60b138f4698f5
LateAlways <90992352+Sky-FoxGAMING@users.noreply.github.com>
2022-08-05 09:09:38 -0400

    Delete getcountry.php

--- getcountry.php
+++
@@ -1,5 +0,0 @@
-<?php
-$ip_add = $_SERVER['REMOTE_ADDR'];
-header('Content-Type: application/json');
-echo "{\"country_name\":\"".json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=".$ip_add))->geoplugin_countryName."\"}";
-?>
