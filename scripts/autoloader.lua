9834b0bb1a05c9bda7e34599ab1c6b2661b1d6fd
LateAlways <90992352+Sky-FoxGAMING@users.noreply.github.com>
2023-02-19 16:29:10 -0500

    Create autoloader.lua

---
+++ scripts/autoloader.lua
@@ -0,0 +1,10 @@
+if not syn and not queue_on_teleport then
+  error("Not supported by your exploit.")
+end
+
+local queueOnTP = syn.queue_on_teleport or queue_on_teleport or error("Unsupported")
+
+local script = ({...})[1]
+
+loadstring(script)()
+queueOnTP("local queueOnTP = syn.queue_on_teleport or queue_on_teleport or error("Unsupported"); loadstring([["..script.."]])(); queueOnTP([["..script.."]]);")
